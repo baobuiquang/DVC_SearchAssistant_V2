3a2953e33de61f6667c89d54a00d4da60c58bad2
Bao Bui-Quang <quangbao.dev@gmail.com>
2025-04-02 14:49:57 +0700

    update

---
+++ build.py
@@ -0,0 +1,37 @@
+import build_config
+import PyInstaller.__main__
+import shutil
+import os
+
+BUILD_DIR = "build"
+
+PyInstaller.__main__.run([
+    f'{build_config.PYTHON_FILE_NAME}',
+    f'--name={build_config.BUILD_NAME}',
+    '--noconsole',
+    '--clean',
+    '--noconfirm',
+    '--onedir',
+    '--log-level=WARN',
+    f'--distpath=./{BUILD_DIR}/dist',
+    f'--workpath=./{BUILD_DIR}/temp',
+    f'--specpath=./{BUILD_DIR}',
+    '--contents-directory=bin',
+])
+
+# Copy folders
+def copy_folder(source, destination):
+    try:
+        if not os.path.exists(source):
+            print(f"Source folder '{source}' does not exist.")
+            return
+        shutil.copytree(source, destination)
+        print(f"Folder copied successfully from '{source}' to '{destination}'.")
+    except FileExistsError:
+        print(f"Destination folder '{destination}' already exists.")
+    except PermissionError:
+        print("Permission denied.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+for fld in build_config.FOLDERS_TO_ADD:
+    copy_folder(f"{fld}", f"{BUILD_DIR}/dist/{build_config.BUILD_NAME}/{fld}")
---
+++ build_config.py
@@ -0,0 +1,8 @@
+PYTHON_FILE_NAME = "main.py"
+
+BUILD_NAME = "DVC_SearchAssistant_V2" # .exe
+
+# These folders will be copied when building exe
+FOLDERS_TO_ADD = [
+    "url",
+]
