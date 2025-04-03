bdae94fab5ede8e2e2521fe43c7f690f11229304
Bao Bui-Quang <quangbao.dev@gmail.com>
2025-04-03 08:09:12 +0700

    update

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
-call python\python.exe -m venv venv
+call py -m venv venv
 call venv\Scripts\activate
-call python --version
-call python -m pip install -U pip
+call py --version
+call py -m pip install -U pip
 call pip install -r requirements.txt
-call python main.py
+call py main.py
 pause
